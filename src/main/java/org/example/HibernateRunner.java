5b39e208df189552e492d9a4fc36bb58830b254c
Leon <ajnj199320@gmail.com>
2023-04-10 14:55:19 +0300

    Custom logger and RollingFileAppender

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,15 +1,14 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.orm.util.HibernateUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class HibernateRunner {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HibernateRunner.class);
     public static void main(String[] args) {
 
 //      The entity status is TRANSIENT(переходный) for whatever session
@@ -18,21 +17,21 @@ public class HibernateRunner {
                 .firstName("someFirstName")
                 .lastName("someLastName")
                 .build();
-        LOGGER.info("User entity status is TRANSIENT for whatever session: {}", user);
+        log.info("User entity status is TRANSIENT for whatever session: {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
             try (session) {
                 Transaction transaction1 = session.beginTransaction();
-                LOGGER.info("Transaction is created: {}", transaction1);
+                log.info("Transaction is created: {}", transaction1);
 //      The entity status is PERSISTENT(стабильный) for the current session
                 session.saveOrUpdate(user);
-                LOGGER.info("User entity: {} has PERSISTENT status for session: {}", user, session);
+                log.info("User entity: {} has PERSISTENT status for session: {}", user, session);
                 transaction1.commit();
             }
-            LOGGER.warn("User entity has DETACHED status for closed session: {}", session);
+            log.warn("User entity has DETACHED status for closed session: {}", session);
         } catch (Exception e) {
-            LOGGER.error("Exception occurred", e);
+            log.error("Exception occurred", e);
             throw e;
         }
 
